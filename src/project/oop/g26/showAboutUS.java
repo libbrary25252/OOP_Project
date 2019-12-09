c151707104980336855f6a7e787d78c7533c80e6
libbrary25252 <n.laiying25@gmail.com>
2019-12-09 18:17:12 +0800

    show about us gui

--- src/project/oop/g26/showAboutUS.java
+++ src/project/oop/g26/showAboutUS.java
@@ -10,7 +10,7 @@ public class showAboutUS extends IPanel{
 
     private showAboutUS() {
         ImageIcon mem = new ImageIcon("Member2.jpg");
-        String Intro = new String("Member:(left to right)" + "\n" + "LIU Tin Nok" + "\n" + "NG Lai Ying" + "\n" +"LIN Ka" + "\n" + "XXX");
+        String Intro = new String("Member:(left to right)" + "\n" + "LIU Tin Nok" + "\n" + "NG Lai Ying" + "\n" +"LIN Ka Hing" + "\n" + "LAM Chak Wai");
         JOptionPane.showMessageDialog(null, Intro, "About Us: G26, CL01, 2019-2020, OOP", JOptionPane.INFORMATION_MESSAGE, mem);
     }
 }

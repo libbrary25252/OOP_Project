a8e20d6b350e94cb0172de1ca1d4b97319450ad0
eric2788 <tsukiko852@gmail.com>
2019-12-10 01:34:41 +0800

    little bug fix

--- src/project/oop/g26/roles/G26IRole.java
+++ src/project/oop/g26/roles/G26IRole.java
@@ -2,29 +2,7 @@ package project.oop.g26.roles;
 
 import project.oop.g26.misc.G26Permission;
 
-import javax.swing.*;
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
-
 public interface G26IRole {
-
-    static String hashPassword(String password) {
-        try {
-            MessageDigest digest = MessageDigest.getInstance("SHA-256");
-            return bytesToHex(digest.digest(password.getBytes()));
-        } catch (NoSuchAlgorithmException e) {
-            JOptionPane.showMessageDialog(null, e.getMessage());
-            return password;
-        }
-    }
-
-    private static String bytesToHex(byte[] hashInBytes) {
-        StringBuilder sb = new StringBuilder();
-        for (byte b : hashInBytes) {
-            sb.append(String.format("%02x", b));
-        }
-        return sb.toString();
-    }
 
     String getUserInfo();
 
